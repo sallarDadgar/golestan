5cc0b80f00c549ee9bc6198a1cb67cc47f53c0eb
sallar <sallar.nsd2.dadgar@gmail.com>
2019-12-05 16:16:18 +0330

    add class scope stuson policy

--- app/policies/stuson_policy.rb
+++ app/policies/stuson_policy.rb
@@ -6,4 +6,21 @@ class StusonPolicy < ApplicationPolicy
   def update?
     user.prof?
   end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin?
+        scope.all
+      elsif user.student?
+        scope.where(fk_id: student.id)
+      end
+    end
+  end
 end
