78e08e9baa33f7365485a12dddae2af4acbd438e
sallar <sallar.nsd2.dadgar@gmail.com>
2019-12-06 20:09:51 +0330

    create app and auth in panels

---
+++ app/javascript/panels/app.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Header />
+    <div class="maindiv" id="app2">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from "../components/header.vue"
+
+  export default {
+    data () {
+      return {
+        message: "Hello Vue"
+      }
+    },
+    components: {
+      Header,
+    }
+  }
+</script>
+
+<style>
+  .maindiv {
+    width: 80%;
+    float: left;
+    height: 100%;
+    }
+  p {
+    font-size: 2em;
+    text-align: center;
+    float: bottom;
+  }
+</style>
---
+++ app/javascript/panels/auth.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="auth">
+    <home />
+  </div>
+</template>
+
+<script>
+import home from "../components/home.vue"
+export default {
+  data () {
+    return {
+      authenticated: false,
+      message: "Hello Vue"
+    }
+  },
+  components: {
+    home
+  }
+}
+</script>
