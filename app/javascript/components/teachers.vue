9a719b568d2173d2b4f67ab1faba516801c5dd09
sallar <sallar.nsd2.dadgar@gmail.com>
2019-12-09 12:14:18 +0330

    complete UI for teachers component and add delete method to it

--- app/javascript/components/teachers.vue
+++ app/javascript/components/teachers.vue
@@ -1,8 +1,107 @@
 <template>
-  <p>this is teachers</p>
+  <div>
+    <router-link to="/add-teachers">
+      <el-button class="addteacher">
+        Add new Professor
+      </el-button>
+    </router-link>
+
+    <el-table
+      :data="users"
+      style="width: 100%"
+      height="450">
+      <el-table-column
+        prop="user.frst_name"
+        label="first name"
+        width="150">
+      </el-table-column>
+      <el-table-column
+        prop="user.last_name"
+        label="last name"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="user.code"
+        label="ID"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="user.email"
+        label="email"
+        width="300">
+      </el-table-column>
+      <el-table-column
+        prop="college"
+        label="college"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="experience"
+        label="experience"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        fixed="right"
+        label="Operations"
+        width="150">
+        <template slot-scope="scope">
+          <router-link to="/edit-teachers">
+            <el-button>
+              <i class="fas fa-edit" title="edit"></i>
+            </el-button>
+          </router-link> |
+          <el-button v-on:click="removeProf(scope.row.id)"
+            onClick="return confirm('are you sure you want to delete this professor?');">
+            <i class="fas fa-trash-alt" title="delete">
+            </i>
+            </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+  </div>
 </template>
 <script>
+import axios from 'axios';
+var deserialize = require('jsonapi-deserializer').deserialize;
 export default {
-
+  data() {
+    return {
+      profs: [],
+      users: []
+    }
+  },
+  methods: {
+    removeProf(id) {
+      this.axios.delete('/profs/' + id)
+      .then((response) => {
+        location.reload()
+      })
+    }
+  },
+  created() {
+    this.axios.get('/profs')
+    .then(response => {
+      this.profs = response.data
+      this.users = deserialize(this.profs);
+    })
+  }
 }
 </script>
+<style>
+.addteacher {
+  width: 300px;
+  height: 40px;
+  left: 50px;
+  top: 40px;
+}
+.techers {
+  width: 20%;
+  float: left;
+}
+.users {
+  height: 100%;
+  width: 80%;
+  float: right;
+}
+</style>
