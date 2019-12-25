f26f570308c0fa40c0f249f4ad727475312ef820
sallar <sallar.nsd2.dadgar@gmail.com>
2019-12-25 18:44:02 +0330

    complete rspec for student policy

--- spec/policies/student_policy_spec.rb
+++ spec/policies/student_policy_spec.rb
@@ -1,7 +1,7 @@
 describe StudentPolicy do
   subject { described_class }
 
-  permissions :index?, :create?, :update?, :show?, :destroy? do
+  permissions :index?, :create?, :update?, :destroy? do
     it "grants access if user is an admin" do
       expect(subject).to permit(User.new(role: :admin))
     end
@@ -9,6 +9,18 @@ describe StudentPolicy do
     it "denies access if user is not admin" do
       expect(subject).not_to permit(User.new(role: :student))
       expect(subject).not_to permit(User.new(role: :prof))
+    end
+  end
+
+
+  permissions :show? do
+    it "grants access if user is an admin or a prof" do
+      expect(subject).to permit(User.new(role: :admin))
+      expect(subject).to permit(User.new(role: :prof))
+    end
+
+    it "denies access if user is student" do
+      expect(subject).not_to permit(User.new(role: :student))
     end
   end
 end
