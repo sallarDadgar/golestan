db1e83bf0beb4b665558d8f360d5780e41500981
sallar <sallar.nsd2.dadgar@gmail.com>
2019-12-25 18:44:49 +0330

    complete rspec for user policy

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -2,13 +2,14 @@ describe UserPolicy do
   subject { described_class }
 
   permissions :index? do
-    it "grants access if user is an admin" do
+    it "grants access if user is an admin or a professor" do
       expect(subject).to permit(User.new(role: :admin))
+      expect(subject).to permit(User.new(role: :prof))
     end
 
     it "denies access if user is not admin" do
       expect(subject).not_to permit(User.new(role: :student))
-      expect(subject).not_to permit(User.new(role: :prof))
+      # expect(subject).not_to permit(User.new(role: :prof))
     end
   end
 end
