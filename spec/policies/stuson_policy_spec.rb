5e3f6dc9d7a7c54652330ddaf98dda7358bce871
sallar <sallar.nsd2.dadgar@gmail.com>
2019-12-25 18:44:21 +0330

    comlete rspec for stuson policy

--- spec/policies/stuson_policy_spec.rb
+++ spec/policies/stuson_policy_spec.rb
@@ -11,13 +11,24 @@ describe StusonPolicy do
     end
   end
 
-  permissions :update? do
+  permissions :update?, :show? do
     it "grants access if user is an prof" do
       expect(subject).to permit(User.new(role: :prof))
     end
 
     it "denies access if user is not prof" do
       expect(subject).not_to permit(User.new(role: :student))
+      expect(subject).not_to permit(User.new(role: :admin))
+    end
+  end
+
+  permissions :index? do
+    it "grants access if user is a prof or a student" do
+      expect(subject).to permit(User.new(role: :prof))
+      expect(subject).to permit(User.new(role: :student))
+    end
+
+    it "denies access if user is admin" do
       expect(subject).not_to permit(User.new(role: :admin))
     end
   end
