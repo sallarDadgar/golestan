7fe2ad0eb5fed5258f7423d5cdf75119c84292b2
sallar <sallar.nsd2.dadgar@gmail.com>
2019-12-25 19:09:11 +0330

    add professor.vue to jsavascript/panels

---
+++ app/javascript/panels/professor.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <TeacherHeader />
+    <div class="maindiv" id="app2">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+  import TeacherHeader from "../components/teacher_header.vue"
+
+  export default {
+    data () {
+      return {
+        message: "Hello Vue"
+      }
+    },
+    components: {
+      TeacherHeader,
+    }
+  }
+</script>
+
+<style>
+  .maindiv {
+    width: 80%;
+    float: left;
+    height: 100%;
+    }
+  p {
+    font-size: 2em;
+    text-align: center;
+    float: bottom;
+  }
+</style>
