559ab2409ae7802307ebe01da491f159c5edc0ac
sallar <sallar.nsd2.dadgar@gmail.com>
2019-12-25 18:43:07 +0330

    complete rspec for field policy

--- spec/policies/field_policy_spec.rb
+++ spec/policies/field_policy_spec.rb
@@ -15,11 +15,11 @@ describe FieldPolicy do
   permissions :index? do
     it "grants access if user is an admin" do
       expect(subject).to permit(User.new(role: :admin))
-      expect(subject).to permit(User.new(role: :student))
+      expect(subject).to permit(User.new(role: :prof))
     end
 
     it "denies access if user is prof" do
-      expect(subject).not_to permit(User.new(role: :prof))
+      expect(subject).not_to permit(User.new(role: :student))
     end
   end
 end
