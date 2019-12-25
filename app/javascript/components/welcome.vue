7f9e14a50214beccbe213bf9f82ad60151538b30
sallar <sallar.nsd2.dadgar@gmail.com>
2019-12-25 18:23:58 +0330

    create welcome component

---
+++ app/javascript/components/welcome.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="upperDiv">
+      <marquee direction="left" scrollamount="25">
+        <marquee direction="right" scrollamount="25">
+          <h1><i><b>ACOM Golestan</b></i></h1>
+        </marquee>
+      </marquee>
+    </div>
+    <div>
+      <marquee direction="left" behavior="alternate" scrollamount="20">
+        <marquee direction="up" behavior="alternate" scrollamount="15">
+          <!-- <marquee direction="right" behavior="alternate" scrollamount="15">
+            <marquee direction="down" behavior="alternate" scrollamount="15"> -->
+              <h2><i><b>welcome</b></i></h2>
+            <!-- </marquee> -->
+          <!-- </marquee> -->
+        </marquee>
+      </marquee>
+    </div>
+  </div>
+</template>
+
+<style>
+h2{
+color: blue;
+}
+.upperDiv{
+  height: 10%;
+  float: top;
+}
+.lowerDiv{
+  height: 90%;
+  float: bottom;
+}
+</style>
