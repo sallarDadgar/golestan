be8d1c7dfd8d20348eaf8539b74c288d13b1fcde
sallar <sallar.nsd2.dadgar@gmail.com>
2019-12-25 18:36:05 +0330

    add fkey_id to user serializer

--- app/serializers/serializable_user.rb
+++ app/serializers/serializable_user.rb
@@ -1,7 +1,7 @@
 class SerializableUser < JSONAPI::Serializable::Resource
   type 'users'
 
-  attributes :frst_name,:profile, :last_name, :code, :role, :created_at, :updated_at, :email
+  attributes :frst_name,:profile, :last_name, :code, :role, :fkey_id, :email
 
   belongs_to :student
   belongs_to :prof
