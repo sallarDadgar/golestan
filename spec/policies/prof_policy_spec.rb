af7eb637bd198dd6f4c98d5095e247f6792c2334
sallar <sallar.nsd2.dadgar@gmail.com>
2019-12-25 18:43:35 +0330

    complete rspec for prof policy

--- spec/policies/prof_policy_spec.rb
+++ spec/policies/prof_policy_spec.rb
@@ -1,13 +1,23 @@
 describe ProfPolicy do
   subject { described_class }
 
-  permissions :index?, :create?, :update?, :show?, :destroy? do
-    it "grants access if user is an admin" do
+  permissions :create?, :update?, :show?, :destroy? do
+    it "grants access if user is an admin or student" do
       expect(subject).to permit(User.new(role: :admin))
     end
 
     it "denies access if user is not admin" do
       expect(subject).not_to permit(User.new(role: :student))
+      expect(subject).not_to permit(User.new(role: :prof))
+    end
+  end
+  permissions :index? do
+    it "grants access if user is an admin or student" do
+      expect(subject).to permit(User.new(role: :admin))
+      expect(subject).to permit(User.new(role: :student))
+    end
+
+    it "denies access if user is not admin" do
       expect(subject).not_to permit(User.new(role: :prof))
     end
   end
