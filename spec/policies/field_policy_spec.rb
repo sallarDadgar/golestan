39410c4201131d773781c5139fe3e4499a2c6ffa
sallar <sallar.nsd2.dadgar@gmail.com>
2019-12-28 00:16:37 +0330

    grant prof access to field#index

--- spec/policies/field_policy_spec.rb
+++ spec/policies/field_policy_spec.rb
@@ -13,13 +13,10 @@ describe FieldPolicy do
   end
 
   permissions :index? do
-    it "grants access if user is an admin" do
+    it "grants access to all users" do
       expect(subject).to permit(User.new(role: :admin))
       expect(subject).to permit(User.new(role: :prof))
-    end
-
-    it "denies access if user is prof" do
-      expect(subject).not_to permit(User.new(role: :student))
+      expect(subject).to permit(User.new(role: :student))
     end
   end
 end
