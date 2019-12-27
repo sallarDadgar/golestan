3d0263469ae55632ed0481649f605314836aeb7a
sallar <sallar.nsd2.dadgar@gmail.com>
2019-12-28 00:19:05 +0330

    add student.vue to javascript/panels

---
+++ app/javascript/panels/student.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <Header />
+    <div class="maindiv" id="app2">
+      <studentInfo />
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from "../components/student_header.vue"
+  import studentInfo from "../components/student_view.vue"
+
+  export default {
+    data () {
+      return {
+        message: "Hello Vue"
+      }
+    },
+    components: {
+      Header,
+      studentInfo
+    }
+  }
+</script>
+
+<style>
+  .maindiv {
+    width: 80%;
+    float: left;
+    height: 100%;
+    }
+  p {
+    font-size: 2em;
+    text-align: center;
+    float: bottom;
+  }
+</style>
